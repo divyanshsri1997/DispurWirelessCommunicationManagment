c097a35ce0fb2cca5edfe469c908e2ec84c1b3b6
Divyansh Srivastava <divyansh@mobiuso.com>
2020-11-18 23:15:11 +0530

    add Admin authentication method

--- TarrifPlanManagementPortal.cs
+++ TarrifPlanManagementPortal.cs
@@ -6,6 +6,23 @@ namespace DispurWirelessCommunicationManagment
 {
     class TarrifPlanManagementPortal
     {
+        public static bool AuthenticateAdmin()
+        {
+            Console.Write("Enter Admin ID: ");
+            int AId = int.Parse(Console.ReadLine());
+            Console.Write("Enter Admin password: ");
+            string pwd = Console.ReadLine();
+            if (AId == 505 && pwd.ToLower() == "rm")
+            {
+                Console.WriteLine("\n\n####....Welcome Admin....####\n");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Wrong credentials....\n\n");
+                return false;
+            }
+        }
         public TarrifPlanManagementPortal()
         {
             while (true)
@@ -22,20 +39,24 @@ namespace DispurWirelessCommunicationManagment
                     case 1:
                         Console.WriteLine("--------------------------------------------------------------------------");
                         Console.WriteLine("Welcome to the Tarrif Plan Page...\n\n");
-                        Console.WriteLine("####Enter the Details####\n");
-                        Console.Write("Enter plan name: ");
-                        string name = Console.ReadLine();
-                        Console.Write("Enter paln type: ");
-                        string type = Console.ReadLine();
-                        Console.Write("Enter tarrif(rate/min): ");
-                        int tarrif = int.Parse(Console.ReadLine());
-                        Console.Write("Enter validity: ");
-                        int validity = int.Parse(Console.ReadLine());
-                        Console.Write("Enter rental(if any): ");
-                        int rental = int.Parse(Console.ReadLine());
-                        TarrifPlan tp = new TarrifPlan(name,type,tarrif,validity,rental);
-                        new TarrifPlanDbAccessLayer().InsertintoTarrifPlanTable(tp);
-                        Console.WriteLine("Please make a note of the Plan ID :" + tp.PlanId);
+                        bool result = AuthenticateAdmin();
+                        if (result)
+                        {
+                            Console.WriteLine("####Enter the Details####\n");
+                            Console.Write("Enter plan name: ");
+                            string name = Console.ReadLine();
+                            Console.Write("Enter paln type: ");
+                            string type = Console.ReadLine();
+                            Console.Write("Enter tarrif(rate/min): ");
+                            int tarrif = int.Parse(Console.ReadLine());
+                            Console.Write("Enter validity: ");
+                            int validity = int.Parse(Console.ReadLine());
+                            Console.Write("Enter rental(if any): ");
+                            int rental = int.Parse(Console.ReadLine());
+                            TarrifPlan tp = new TarrifPlan(name, type, tarrif, validity, rental);
+                            new TarrifPlanDbAccessLayer().InsertintoTarrifPlanTable(tp);
+                            Console.WriteLine("Please make a note of the Plan ID :" + tp.PlanId);
+                        }
                         Console.WriteLine("--------------------------------------------------------------------------");
                         break;
                     case 2:
@@ -47,21 +68,12 @@ namespace DispurWirelessCommunicationManagment
                     case 3:
                         Console.WriteLine("--------------------------------------------------------------------------");
                         Console.WriteLine("Remove Plan Page...\n\n");
-                        Console.Write("Enter Admin ID: ");
-                        int AId = int.Parse(Console.ReadLine());
-                        Console.Write("Enter Admin password: ");
-                        string pwd = Console.ReadLine();
-                        if (AId == 505 && pwd.ToLower() == "rm")
+                        bool isAuthencated = AuthenticateAdmin();
+                        if(isAuthencated)
                         {
-                            Console.WriteLine("\n\n####....Welcome Admin....####\n");
                             Console.Write("Enter plan ID: ");
                             int id = int.Parse(Console.ReadLine());
                             new TarrifPlanDbAccessLayer().RemovePlan(id);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Wrong credentials....\n\n");
-                            break;
                         }
                         
                         Console.WriteLine("--------------------------------------------------------------------------");
@@ -69,23 +81,14 @@ namespace DispurWirelessCommunicationManagment
                     case 4:
                         Console.WriteLine("--------------------------------------------------------------------------");
                         Console.WriteLine("Update Tarrif Page...\n\n");
-                        Console.Write("Enter Admin ID: ");
-                        int AdminId = int.Parse(Console.ReadLine());
-                        Console.Write("Enter Admin password: ");
-                        string pw = Console.ReadLine();
-                        if (AdminId == 505 && pw.ToLower() == "rm")
+                        bool isAdmin = AuthenticateAdmin();
+                        if(isAdmin)
                         {
-                            Console.WriteLine("\n\n####....Welcome Admin....####\n");
                             Console.Write("Enter plan ID: ");
                             int id1 = int.Parse(Console.ReadLine());
                             Console.Write("Enter new tarrif: ");
                             int t = int.Parse(Console.ReadLine());
                             new TarrifPlanDbAccessLayer().UpdateTarrif(id1, t);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Wrong credentials....\n\n");
-                            break;
                         }
                         Console.WriteLine("--------------------------------------------------------------------------");
                         break;
